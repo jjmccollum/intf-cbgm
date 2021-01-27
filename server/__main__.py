df2f85a3f15fd031d104e2029451e3c6ed16c7b7
Dennis Voltz <dennis.voltz@wwu.de>
2021-01-27 17:28:52 +0100

    Fix intendations

--- server/__main__.py
+++ server/__main__.py
@@ -45,45 +45,45 @@ import editor
 import set_cover
 import checks
 
-dba = flask_sqlalchemy.SQLAlchemy ()
-user, _role, _roles_users = login.declare_user_model_on (dba)
-db_adapter = flask_user.SQLAlchemyAdapter (dba, user)
-login_manager = flask_login.LoginManager ()
+dba = flask_sqlalchemy.SQLAlchemy()
+user, _role, _roles_users = login.declare_user_model_on(dba)
+db_adapter = flask_user.SQLAlchemyAdapter(dba, user)
+login_manager = flask_login.LoginManager()
 login_manager.anonymous_user = login.AnonymousUserMixin
-user_manager = flask_user.UserManager (db_adapter)
-mail = flask_mail.Mail ()
+user_manager = flask_user.UserManager(db_adapter)
+mail = flask_mail.Mail()
 
 
 class Config ():
     """ Default configuration object. """
 
-    APPLICATION_HOST    = 'localhost'
-    APPLICATION_PORT    = 5000
+    APPLICATION_HOST = 'localhost'
+    APPLICATION_PORT = 5000
     APPLICATION_DESCRIPTION = ''
-    CONFIG_FILE         = '_global.conf' # default = ./instance/_global.conf
-    STATIC_FOLDER       = 'static'
-    STATIC_URL_PATH     = 'static'
-    AFTER_LOGIN_URL     = None
-    USE_RELOADER        = False
-    USE_DEBUGGER        = False
-    SERVER_START_TIME   = str (int (time.time ())) # for cache busting
-    READ_ACCESS         = 'none'
+    CONFIG_FILE = '_global.conf'  # default = ./instance/_global.conf
+    STATIC_FOLDER = 'static'
+    STATIC_URL_PATH = 'static'
+    AFTER_LOGIN_URL = None
+    USE_RELOADER = False
+    USE_DEBUGGER = False
+    SERVER_START_TIME = str(int(time.time()))  # for cache busting
+    READ_ACCESS = 'none'
     READ_ACCESS_PRIVATE = 'none'
-    WRITE_ACCESS        = 'none'
-    CORS_ALLOW_ORIGIN   = '*'
+    WRITE_ACCESS = 'none'
+    CORS_ALLOW_ORIGIN = '*'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-def build_parser (default_config_file = Config.CONFIG_FILE):
+def build_parser(default_config_file=Config.CONFIG_FILE):
     """ Build the commandline parser. """
 
-    parser = argparse.ArgumentParser (description = __doc__)
+    parser = argparse.ArgumentParser(description=__doc__)
 
-    parser.add_argument (
+    parser.add_argument(
         '-v', '--verbose', dest='verbose', action='count',
         help='increase output verbosity', default=0
     )
-    parser.add_argument (
+    parser.add_argument(
         '-c', '--config-file', dest='config_file',
         default=default_config_file, metavar='CONFIG_FILE',
         help="the config file (default='./instance/%s')" % default_config_file
@@ -91,25 +91,25 @@ def build_parser (default_config_file = Config.CONFIG_FILE):
     return parser
 
 
-def do_init_app (app):
+def do_init_app(app):
     """ Initializations to do on main and sub apps. """
 
-    app.config['APPLICATION_ROOT'] = app.config['APPLICATION_ROOT'].rstrip ('/')
+    app.config['APPLICATION_ROOT'] = app.config['APPLICATION_ROOT'].rstrip('/')
 
-    dba.init_app (app)
-    mail.init_app (app)
-    login.init_app (app)
-    user_manager.init_app (app, login_manager = login_manager,
-                           make_safe_url_function = login.make_safe_url)
+    dba.init_app(app)
+    mail.init_app(app)
+    login.init_app(app)
+    user_manager.init_app(app, login_manager=login_manager,
+                          make_safe_url_function=login.make_safe_url)
 
-    @app.errorhandler (EditException)
-    def handle_invalid_edit (ex):
-        response = flask.jsonify (ex.to_dict ())
+    @app.errorhandler(EditException)
+    def handle_invalid_edit(ex):
+        response = flask.jsonify(ex.to_dict())
         response.status_code = ex.status_code
         return response
 
     @app.after_request
-    def add_headers (response):
+    def add_headers(response):
         response.headers['Access-Control-Allow-Origin'] = current_app.config['CORS_ALLOW_ORIGIN']
         response.headers['Access-Control-Allow-Credentials'] = 'true'
         # response.headers['Cache-Control'] = 'private, max-age=3600'
@@ -117,88 +117,88 @@ def do_init_app (app):
         response.headers['Server'] = 'Jetty 0.8.15'
         return response
 
-    app.logger.info ("Mounted {name} at {host}:{port}{mount} from conf {conf}".format (
-        name  = app.config['APPLICATION_NAME'],
-        host  = app.config['APPLICATION_HOST'],
-        port  = app.config['APPLICATION_PORT'],
-        mount = app.config['APPLICATION_ROOT'],
-        conf  = app.config['CONFIG_FILE']
+    app.logger.info("Mounted {name} at {host}:{port}{mount} from conf {conf}".format(
+        name=app.config['APPLICATION_NAME'],
+        host=app.config['APPLICATION_HOST'],
+        port=app.config['APPLICATION_PORT'],
+        mount=app.config['APPLICATION_ROOT'],
+        conf=app.config['CONFIG_FILE']
     ))
 
 
-def create_app (Config):
+def create_app(Config):
     """ App creation function """
 
-    instance_path = os.path.abspath ('instance')
+    instance_path = os.path.abspath('instance')
 
-    app = flask.Flask (__name__)
+    app = flask.Flask(__name__)
 
-    global_config = os.path.join (instance_path, Config.CONFIG_FILE)
-    app.config.from_object (Config)
-    app.config.from_pyfile (global_config)
+    global_config = os.path.join(instance_path, Config.CONFIG_FILE)
+    app.config.from_object(Config)
+    app.config.from_pyfile(global_config)
 
     # pylint: disable=no-member
-    app.logger.setLevel (Config.LOG_LEVEL)
-    app.logger.info ("Instance path: {ip}".format (ip = instance_path))
+    app.logger.setLevel(Config.LOG_LEVEL)
+    app.logger.info("Instance path: {ip}".format(ip=instance_path))
 
-    app.register_blueprint (static.bp)
-    app.register_blueprint (login.bp)
+    app.register_blueprint(static.bp)
+    app.register_blueprint(login.bp)
 
-    app.config.dba = db_tools.PostgreSQLEngine (**app.config)
+    app.config.dba = db_tools.PostgreSQLEngine(**app.config)
     user_db_url = app.config.dba.url
     # tell flask_sqlalchemy where the user authentication database is
     app.config['SQLALCHEMY_DATABASE_URI'] = user_db_url
 
-    static.init_app (app)
-    do_init_app (app)
+    static.init_app(app)
+    do_init_app(app)
 
-    instances = collections.OrderedDict ()
+    instances = collections.OrderedDict()
     extra_files = [instance_path + '/' + Config.CONFIG_FILE]
 
-    for fn in glob.glob (instance_path + '/*.conf'):
-        extra_files.append (fn)
-        fn = os.path.basename (fn)
+    for fn in glob.glob(instance_path + '/*.conf'):
+        extra_files.append(fn)
+        fn = os.path.basename(fn)
         if fn == Config.CONFIG_FILE:
             continue
 
-        sub_app = flask.Flask (__name__)
-        sub_app.config.from_object (Config)
-        sub_app.config.from_pyfile (global_config)
-        sub_app.config.from_pyfile (os.path.join (instance_path, fn))
+        sub_app = flask.Flask(__name__)
+        sub_app.config.from_object(Config)
+        sub_app.config.from_pyfile(global_config)
+        sub_app.config.from_pyfile(os.path.join(instance_path, fn))
         sub_app.config['CONFIG_FILE'] = fn
         sub_app.config['APPLICATION_DIR'] = sub_app.config['APPLICATION_ROOT']
-        sub_app.config['APPLICATION_ROOT'] = os.path.join (
+        sub_app.config['APPLICATION_ROOT'] = os.path.join(
             app.config['APPLICATION_ROOT'], sub_app.config['APPLICATION_ROOT']
         )
-        sub_app.register_blueprint (main.bp)
-        sub_app.register_blueprint (textflow.bp)
-        sub_app.register_blueprint (comparison.bp)
-        sub_app.register_blueprint (editor.bp)
-        sub_app.register_blueprint (set_cover.bp)
-        sub_app.register_blueprint (checks.bp)
+        sub_app.register_blueprint(main.bp)
+        sub_app.register_blueprint(textflow.bp)
+        sub_app.register_blueprint(comparison.bp)
+        sub_app.register_blueprint(editor.bp)
+        sub_app.register_blueprint(set_cover.bp)
+        sub_app.register_blueprint(checks.bp)
 
-        sub_app.config.dba = db_tools.PostgreSQLEngine (**sub_app.config)
+        sub_app.config.dba = db_tools.PostgreSQLEngine(**sub_app.config)
         sub_app.config['SQLALCHEMY_DATABASE_URI'] = user_db_url
 
-        do_init_app (sub_app)
-        main.init_app (sub_app)
-        textflow.init_app (sub_app)
-        comparison.init_app (sub_app)
-        editor.init_app (sub_app)
-        set_cover.init_app (sub_app)
-        checks.init_app (sub_app)
+        do_init_app(sub_app)
+        main.init_app(sub_app)
+        textflow.init_app(sub_app)
+        comparison.init_app(sub_app)
+        editor.init_app(sub_app)
+        set_cover.init_app(sub_app)
+        checks.init_app(sub_app)
 
         instances[sub_app.config['APPLICATION_ROOT']] = sub_app
 
-    info_app = flask.Flask (__name__)
-    info_app.config.update (app.config)
-    info_app.register_blueprint (info.bp)
-    do_init_app (info_app)
-    info.init_app (app, instances)
+    info_app = flask.Flask(__name__)
+    info_app.config.update(app.config)
+    info_app.register_blueprint(info.bp)
+    do_init_app(info_app)
+    info.init_app(app, instances)
 
     instances[app.config['APPLICATION_ROOT']] = info_app
 
-    d = DispatcherMiddleware (app, instances)
+    d = DispatcherMiddleware(app, instances)
     d.config = app.config
     d.config['EXTRA_FILES'] = extra_files
     return d
@@ -207,22 +207,22 @@ def create_app (Config):
 if __name__ == "__main__":
     from werkzeug.serving import run_simple
 
-    build_parser ().parse_args (namespace = args)
-    init_logging (
+    build_parser().parse_args(namespace=args)
+    init_logging(
         args,
         flask.logging.default_handler,
-        logging.FileHandler ('server.log')
+        logging.FileHandler('server.log')
     )
 
-    Config.LOG_LEVEL   = args.log_level
+    Config.LOG_LEVEL = args.log_level
     Config.CONFIG_FILE = args.config_file
-    app = create_app (Config)
+    app = create_app(Config)
 
-    run_simple (
+    run_simple(
         app.config['APPLICATION_HOST'],
         app.config['APPLICATION_PORT'],
         app,
-        use_reloader = app.config['USE_RELOADER'],
-        use_debugger = app.config['USE_DEBUGGER'],
-        extra_files  = app.config['EXTRA_FILES']
+        use_reloader=app.config['USE_RELOADER'],
+        use_debugger=app.config['USE_DEBUGGER'],
+        extra_files=app.config['EXTRA_FILES']
     )
--- server/info.py
+++ server/info.py
@@ -11,19 +11,20 @@ import flask_login
 from helpers import make_json_response
 from login import user_can_read, user_can_write
 
-bp = flask.Blueprint ('info', __name__)
+bp = flask.Blueprint('info', __name__)
 
-instances = collections.OrderedDict ()
+instances = collections.OrderedDict()
 
-def init_app (app, instances_):
+
+def init_app(app, instances_):
     """ Initialize the flask app. """
 
     global instances
-    instances.update (instances_)
+    instances.update(instances_)
 
 
-@bp.route ('/user.json')
-def user_json ():
+@bp.route('/user.json')
+def user_json():
     """Endpoint.  Serve information about the current user."""
 
     user = flask_login.current_user
@@ -32,42 +33,43 @@ def user_json ():
     if logged_in:
         roles += [r.name for r in user.roles]
 
-    return make_json_response ({
-        'username'  : user.username if logged_in else 'anonymous',
-        'roles'     : roles,
-        'can_login' : current_app.config['AFTER_LOGIN_URL'] is not None
+    return make_json_response({
+        'username': user.username if logged_in else 'anonymous',
+        'roles': roles,
+        'can_login': current_app.config['AFTER_LOGIN_URL'] is not None
     })
 
 
-@bp.route ('/info.json')
-def index ():
+@bp.route('/info.json')
+def index():
     """Endpoint.  Serve general info about all registered apps."""
 
-    def copy (a):
+    def copy(a):
         return {
-            'application_name'        : a.config['APPLICATION_NAME'],
-            'application_root'        : a.config['APPLICATION_DIR'].rstrip ('/') + '/',
-            'application_description' : a.config['APPLICATION_DESCRIPTION'],
-            'user_can_write'          : user_can_write (a),
+            'application_name': a.config['APPLICATION_NAME'],
+            'application_root': a.config['APPLICATION_DIR'].rstrip('/') + '/',
+            'application_description': a.config['APPLICATION_DESCRIPTION'],
+            'user_can_write': user_can_write(a),
         }
 
-    apps = sorted (instances.values (), key = lambda a: a.config['APPLICATION_NAME'])
+    apps = sorted(instances.values(),
+                  key=lambda a: a.config['APPLICATION_NAME'])
 
-    return make_json_response ({
-        'instances' : [copy (app) for app in apps if user_can_read (app)],
+    return make_json_response({
+        'instances': [copy(app) for app in apps if user_can_read(app)],
     })
 
 
-@bp.route ('/messages.json')
-def messages_json ():
+@bp.route('/messages.json')
+def messages_json():
     """Endpoint.  Serve the flashed messages."""
 
-    return make_json_response ({
-        'messages' : [
+    return make_json_response({
+        'messages': [
             {
-                'message'  : m[1],
-                'category' : m[0],
+                'message': m[1],
+                'category': m[0],
             }
-            for m in (flask.get_flashed_messages (with_categories = True) or [])
+            for m in (flask.get_flashed_messages(with_categories=True) or [])
         ]
     })
