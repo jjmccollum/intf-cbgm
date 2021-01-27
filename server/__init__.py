9370a0611a20924a0275bb6ed58e7f9a8baf71ec
Dennis Voltz <dennis.voltz@wwu.de>
2021-01-27 17:29:01 +0100

    Add sys path to start flask locally

--- server/__init__.py
+++ server/__init__.py
@@ -1,3 +1,7 @@
 """This package implements the application server for CBGM.
 
 """
+import os
+import sys
+# add this dir to PYTHONPATH; on live server this is handled via systemctl
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
